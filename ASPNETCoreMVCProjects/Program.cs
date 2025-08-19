164c68a1997cf7b9661bda181b584d0274c93f8e
lemongautam79 <lemongautam79@gmail.com>
2025-08-19 16:54:16 +0545

    [Employee done]

--- ASPNETCoreMVCProjects/Program.cs
+++ ASPNETCoreMVCProjects/Program.cs
@@ -6,24 +6,30 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+//app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    //pattern: "{controller=Home}/{action=Index}/{id?}")
+    //.WithStaticAssets();
+    pattern: "{controller=Employee}/{action=Create}/{id?}");
+   
 
 
 app.Run();
+
+
+
