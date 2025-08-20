55d897578f32efd0f22d9403c272668d3851cb0a
Piyush Bhattarai <bhattarai.piyush.14@gmail.com>
2025-08-20 22:19:21 +0545

    [Patient Registration Form]

---
+++ ASPNETCoreMVCProjects/Controllers/PatientController.cs
@@ -0,0 +1,54 @@
+﻿using ASPNETCoreMVCProjects.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASPNETCoreMVCProjects.Controllers
+{
+    public class PatientController : Controller
+    {
+        // Static in-memory list to store patients
+        private static List<Patient> patients = new List<Patient>();
+
+        // GET: Display form and submitted patients
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new PatientViewModel
+            {
+                // Show latest patients first
+                Patients = patients.OrderByDescending(p => p.Id).ToList()
+            };
+            return View(model);
+        }
+
+        // POST: Accept form submission
+        [HttpPost]
+        public IActionResult Create(Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add patient to in-memory list
+                patients.Add(patient);
+            }
+
+            var model = new PatientViewModel
+            {
+                Patients = patients.OrderByDescending(p => p.Id).ToList(),
+                LatestPatientId = patient.Id // Highlight latest
+            };
+
+            return View(model);
+        }
+    }
+
+    // ViewModel for form + list
+    public class PatientViewModel
+    {
+        public Patient Patient { get; set; } = new Patient();
+
+        public List<Patient> Patients { get; set; } = new List<Patient>();
+
+        public int LatestPatientId { get; set; } = 0;
+    }
+}
