12168037cbbcece8a99b255b4e61e73e33aeddc9
lemongautam79 <lemongautam79@gmail.com>
2025-08-20 17:47:52 +0545

    [Online Quiz Submission]

---
+++ ASPNETCoreMVCProjects/Controllers/QuizController.cs
@@ -0,0 +1,52 @@
+﻿
+using ASPNETCoreMVCProjects.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreMVCProjects.Controllers
+{
+    public class QuizController : Controller
+    {
+        // Static in memory list to store Quiz data
+        private static List<Quiz> quizes = new List<Quiz>();
+
+        //GET: Display form and submitted quizes
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var viewModel = new QuizViewModel
+            {
+                Quizes = quizes.OrderByDescending(q => q.Score).ToList(),
+            };
+            return View(viewModel);
+        }
+
+        // POST: Accept form submission
+        [HttpPost]
+        public IActionResult Create(Quiz quiz)
+        {
+            if (ModelState.IsValid)
+            {
+                quizes.Add(quiz);
+            }
+
+            var model = new QuizViewModel
+            {
+                Quizes = quizes.OrderByDescending(q => q.Score).ToList(),
+                LatestQuizId = quiz.Id
+            };
+            return View(model);
+        }
+    }
+
+    // View Model to combine form data and list of quizes
+    public class QuizViewModel
+    {
+        //Form Input
+        public Quiz Quiz { get; set; } = new Quiz();
+
+        //List all quizes
+        public List<Quiz> Quizes { get; set; } = new List<Quiz>();
+
+        public int LatestQuizId { get; set; } = 0;
+    }
+}
---
+++ ASPNETCoreMVCProjects/Models/Quiz.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETCoreMVCProjects.Models
+{
+    public class Quiz
+    {
+        // Student Id is required
+        [Required(ErrorMessage = "Student Id is required")]
+        public int Id { get; set; }
+
+        // Student Name is required
+        [Required(ErrorMessage = "Student Name is required")]
+        public string Name { get; set; }
+
+        //Score is required
+        [Required(ErrorMessage = "Score is required")]
+        public decimal Score { get; set; }
+    }
+}
