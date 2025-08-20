faafbd74a6afcab4afbd290c7b9c4dc41316c216
lemongautam79 <lemongautam79@gmail.com>
2025-08-20 08:32:52 +0545

    [Movie Ticket Booking]

---
+++ ASPNETCoreMVCProjects/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETCoreMVCProjects.Models
+{
+    public class Movie
+    {
+
+        //Id is required
+        [Required(ErrorMessage = "Id is required")]
+        public int Id { get; set; }
+
+        //Name is required
+        [Required(ErrorMessage = "Movie name is required")]
+        public string MovieName { get; set; }
+
+        //Seat Number is required
+        [Required(ErrorMessage = "Seat Number is required")]
+        public int SeatNumber { get; set; }
+
+        // Price is required
+        [Required(ErrorMessage = "Price is required")]
+        public decimal Price { get; set; }
+
+    }
+}
