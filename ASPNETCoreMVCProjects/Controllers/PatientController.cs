d1a1641705de19a2ea16d9e6f7e0488698438bfc
Piyush Bhattarai <bhattarai.piyush.14@gmail.com>
2025-08-20 18:07:47 +0545

    patients

---
+++ ASPNETCoreMVCProjects/Controllers/PatientController.cs
@@ -0,0 +1,53 @@
+﻿using ASPNETCoreMVCProjects.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreMVCProjects.Controllers
+{
+    public class PatientController : Controller
+    {
+        // Static in memory list to store patient data
+        private static List<Patient> patients = new List<Patient>();
+
+
+        // GET: Display form and submitted patients
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new PatientViewModel
+            {
+                //Show patients latest first
+                Patients = patients.OrderByDescending(e => e.Id).ToList(),
+            };
+            return View(model);
+        }
+
+        //POST: Accept form submission
+        [HttpPost]
+        public IActionResult Create(Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add patient to the in memory list
+                patients.Add(patient);
+            }
+            var model = new PatientViewModel
+            {
+                //Show patients latest first
+                Patients = patients.OrderByDescending(e => e.Id).ToList(),
+                LatestPatientId = patient.Id // Set the latest patient ID for highlighting
+            };
+            return View(model);
+        }
+    }
+
+    public class PatientViewModel
+    {
+        public Patient Patient { get; set; } = new Patient();
+
+        //List of all patients
+        public List<Patient> Patients { get; set; } = new List<Patient>();
+
+        //ID of the latest student for highlighting
+        public int LatestPatientId { get; set; } = 0;
+    }
+}
---
+++ ASPNETCoreMVCProjects/Models/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETCoreMVCProjects.Models
+{
+    public class Patient
+    {
+        [Required(ErrorMessage = "Patient ID is required")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Age is required")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Disease is required")]
+        public string Disease { get; set; }
+    }
+}
