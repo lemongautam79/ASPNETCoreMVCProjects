3478391c2eb0bce6f2425da9b094bf4b0fddd808
aashishneupane07 <itsmeaashish67@gmail.com>
2025-08-21 21:42:14 +0545

    restaurant order management

---
+++ ASPNETCoreMVCProjects/Controllers/RestaurantController.cs
@@ -0,0 +1,56 @@
+﻿using ASPNETCoreMVCProjects.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreMVCProjects.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        //Static in memory list to share orders data
+        private static List<Restaurant> restaurants = new List<Restaurant>();
+
+        //GET: Display form and submitted restaurant orders
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var orderedItems = restaurants.OrderByDescending(r => r.Id).ToList();
+            var totalPrice = orderedItems.Sum(r => r.Price * r.Quantity);
+            var model = new RestaurantViewModel
+            {
+                //Show latest order first
+                Restaurants = orderedItems,
+                TotalPrice = totalPrice
+            };
+            return View(model);
+        }
+        //POST: Accept form submission
+        [HttpPost]
+        public IActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                restaurants.Add(restaurant);
+            }
+            var orderedItems = restaurants.OrderByDescending(r => r.Id).ToList();
+            var totalPrice = orderedItems.Sum(r => r.Price * r.Quantity);
+            var model = new RestaurantViewModel
+            {
+                Restaurants = orderedItems,
+                TotalPrice = totalPrice,
+                LatestRestaurantId = restaurant.Id
+            };
+            return View(model);
+        }
+    }
+    // View Model to combine form data and list of restaurant orders
+    public class RestaurantViewModel
+    {
+        //Form Input
+        public Restaurant Restaurant { get; set; } = new Restaurant();
+        //List of restaurant orders
+        public List<Restaurant> Restaurants { get; set; } = new List<Restaurant>();
+        //Total price of all orders
+        public decimal TotalPrice { get; set; } = 0;
+        //Id of the latest order for highlighting
+        public int LatestRestaurantId { get; set; } = 0;
+    }
+}
---
+++ ASPNETCoreMVCProjects/Models/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETCoreMVCProjects.Models
+{
+    public class Restaurant
+    {
+        //Id is required
+        [Required(ErrorMessage = "Order id is required")]
+        public int Id { get; set; }
+        //Customer Name is required
+        [Required(ErrorMessage = "Customer Name is required")]
+        public string CustomerName { get; set; }
+        //Food Item Name is required
+        [Required(ErrorMessage = "Food Item Name is required")]
+        public string FoodItemName { get; set; }
+        // Quantity is required
+        [Required(ErrorMessage = "Quantity is required")]
+        public int Quantity { get; set; }
+        // Price is required
+        [Required(ErrorMessage = "Price is required")]
+        public decimal Price { get; set; }
+    }
+}
