a87a6fc78620c61f9e45ed130e69d03745572af1
lemongautam79 <lemongautam79@gmail.com>
2025-08-21 09:02:41 +0545

    [Book Library System]

---
+++ ASPNETCoreMVCProjects/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETCoreMVCProjects.Models
+{
+    public class Book
+    {
+        // Book id is required
+        [Required(ErrorMessage = "Book id is required")]
+        public int BookId { get; set; }
+
+        // Title is required
+        [Required(ErrorMessage ="Book Title is required")]
+        public string Title { get; set; }
+
+        //Author Name is required
+        [Required(ErrorMessage ="Author name is required")]
+        public string Author { get; set; }
+
+        //Publication Year is required
+        [Required(ErrorMessage ="Publication Year is required")]
+        [Range(1000,2100)]
+        public int PublicationYear { get; set; }
+
+    }
+}
