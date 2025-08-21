df4afcbe4765555911246afd6c7b42072a6ae9e8
Piyush Bhattarai <bhattarai.piyush.14@gmail.com>
2025-08-21 21:24:54 +0545

    flight reservation

---
+++ ASPNETCoreMVCProjects/Models/Flight.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETCoreMVCProjects.Models
+{
+    public class Flight
+    {
+        [Required(ErrorMessage = "Reservation Id is required")]
+        public int ReservationId { get; set; }
+
+        [Required(ErrorMessage = "Passenger Name is required")]
+        public string PassengerName { get; set; }
+
+        [Required(ErrorMessage = "Flight Number is required")]
+        public string FlightNumber { get; set; }
+
+        [NotPastDate]
+        [DataType(DataType.Date)]
+        public DateTime FlightDate { get; set; } = DateTime.Today;
+    }
+}
+
+
